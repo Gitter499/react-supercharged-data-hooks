ed38e96c8a293b9115a51ea077d41806e387c492
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-09-08 17:53:15 +0000

    Added testing script

---
+++ src/useSuperchargedObject.ts
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export const useSuperchargedObject = (defaultValue: Object) => {
+
+    class SuperchargedObject {
+        push(): void {
+            
+        }
+    }
+}
